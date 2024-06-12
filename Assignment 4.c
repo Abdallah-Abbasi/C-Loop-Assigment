6a0888dd1c6f6c5f28f6650bc1f1fa5a4e974544
Abdallah-Abbasi <abdallahabbasi332004@gmail.com>
2024-06-12 18:55:35 +0300

    added first question section A

---
+++ Assignment 4.c
@@ -0,0 +1,9 @@
+//Question 1
+//A
+/*
+An armstrong number is a n-digits number that is equal to the sum of the nth
+power of its digits.
+Examples :
+6 = 61 = 6 (n=1 is the number of digits in 6)
+371 = 33 + 73 + 13 = 371 ( n=3 is the number of digits in of 371)
+*/
